87e60b0a5661ff838fc01919587a4dfea23f6c18
InvisiBug <matthewjkavanagh@gmail.com>
2020-11-07 22:02:31 +0000

    Added comments to mqtt connections

--- src/index.js
+++ src/index.js
@@ -66,10 +66,11 @@ global.io = require("socket.io")(server);
 //
 ////////////////////////////////////////////////////////////////////////
 const mqtt = require("mqtt");
-// global.client = mqtt.connect("mqtt://192.168.1.46");
-// global.client = mqtt.connect("mqtt://kavanet.io");
 
-global.client = mqtt.connect("mqtt://localhost");
+// global.client = mqtt.connect("mqtt://192.168.1.46"); // Internal network
+// global.client = mqtt.connect("mqtt://kavanet.io"); // Internet
+global.client = mqtt.connect("mqtt://localhost"); // Laptop
+
 client.setMaxListeners(16); // Disables event listener warning
 
 client.subscribe("#", (err) => {
