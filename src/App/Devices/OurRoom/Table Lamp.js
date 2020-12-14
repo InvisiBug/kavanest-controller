1e7cd756ca0bf4ae40113f8306c8781809dc633e
InvisiBug <matthewjkavanagh@gmail.com>
2020-12-14 15:31:58 +0000

    Fixed table lamp reporting the wrong colour

--- src/App/Devices/OurRoom/Table Lamp.js
+++ src/App/Devices/OurRoom/Table Lamp.js
@@ -95,7 +95,7 @@ client.on("message", (topic, payload) => {
       deviceData = {
         ...deviceData,
         isConnected: true,
-        red: 20,
+        red: mqttData.red,
         green: mqttData.green,
         blue: mqttData.blue,
         mode: mqttData.mode,
